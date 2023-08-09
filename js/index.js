ad0d6a707527b5d771f8cf07e60db15de19b7db9
gyusang <monet000414@naver.com>
2023-08-10 01:05:29 +0900

    main 구현 완료

--- js/index.js
+++ js/index.js
@@ -18,4 +18,35 @@ function changeSlide() {
 setInterval(changeSlide, 2000);
 });
 
+// article 반복문
+
+const container = document.querySelector('.rankingTitle');
+
+for (let i = 1; i<=5; i++){
+    const article = document.createElement('article');
+    article.innerHTML = `
+<article>
+    <a href="#">
+        <div class="ranking"><em>${i}</em></div>
+            <img src="./img/logo.jpeg" alt="로고" />
+                <div class="ranking-title">
+                    <div>
+                        <span>어느 먹방 유튜버의 인성</span><p>[2${i}]</p>
+                    </div>
+                <div>
+                    <h6>1${i}시간 전</h6><h6>왕 귀 력${i}</h6>
+                </div>
+        </div>
+    </a>
+</article>
+`;
+
+if(i === 5){
+    article.querySelector('a').style.borderBottom = 'none';
+}
+
+container.appendChild(article);
+}
+
+
 
